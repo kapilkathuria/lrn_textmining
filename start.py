92bb394b260dd28540d93d31b58eb736a0dd8533
Kapil Kathuria <kapil.kathuria@gmail.com>
2020-04-10 10:00:10 +0530

    Week 1 - Video 3 In progress

--- start.py
+++ start.py
@@ -1,2 +1,21 @@
 # Learning text mining
 # Course https://www.coursera.org/learn/python-text-mining/home/welcome
+
+# Week 1 - Working with Text in Python
+
+# Parse / Find / Indetify / Extract 
+# Classify / Sentiment
+# Topic Modelling
+
+# Premitive constructs - sentences --> Words--> Chartacters
+
+text1 = "Kapil is learning text mining"
+print(len(text1))
+
+# Tockens
+text2 = text1.split(' ')
+print(text2)
+
+# Find Specific
+print(w for w in text2)
+
